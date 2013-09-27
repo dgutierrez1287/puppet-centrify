a8da8bc97bcbe71abb4827d58d12caeb6b8605a1
diego <diego@localhost>
2013-09-26 22:57:10 -0400

    ADDED: some additional params for the adjoin such as username password and domain to join too
    FIXED: some linting

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,17 @@
 
 class centrify::params {
-  
+
   $dc_package_name     = 'CentrifyDC'
   $dc_package_ensure   = 'present'
   $dc_service_name     = 'centrifydc'
   $dc_service_enable   = true
-  $dc_service_ensure   = 'running' 
+  $dc_service_ensure   = 'running'
   $ssh_package_name    = 'CentrifyDC-openssh'
   $ssh_package_ensure  = 'present'
   $ssh_service_name    = 'centrify-sshd'
   $ssh_service_enable  = true
   $ssh_service_ensure  = 'running'
-  $auth_servers        = [] 
+  $auth_servers        = []
   $ssh_permit_root     = 'no'
   $ssh_port            = '22'
   $ssh_x11             = 'no'
@@ -19,4 +19,7 @@ class centrify::params {
   $groups_allow        = []
   $users_allow         = []
   $domain_name         = ''
+  $adjoin_user         = ''
+  $adjoin_password     = ''
+  $adjoin_domain       = $domain_name
 }
