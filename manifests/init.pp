f2e60e94d707aae7afbd9d7151c52c22a8317fb8
Diego Gutierrez <dgutierrez1287@gmail.com>
2014-08-29 15:18:33 -0400

    MODIFIED: added those new params and added validation for them

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,34 +33,35 @@
 #
 #
 class centrify (
-  $dc_package_name     = $centrify::params::dc_package_name,
-  $dc_package_ensure   = $centrify::params::dc_package_ensure,
-  $dc_service_name     = $centrify::params::dc_service_name,
-  $dc_service_enable   = $centrify::params::dc_service_enable,
-  $dc_service_ensure   = $centrify::params::dc_service_ensure,
-  $ssh_package_name    = $centrify::params::ssh_package_name,
-  $ssh_package_ensure  = $centrify::params::ssh_package_ensure,
-  $ssh_service_name    = $centrify::params::ssh_service_name,
-  $ssh_service_enable  = $centrify::params::ssh_service_enable,
-  $ssh_service_ensure  = $centrify::params::ssh_service_ensure,
-  $auth_servers        = $centrify::params::auth_servers,
-  $ssh_permit_root     = $centrify::params::ssh_permit_root,
-  $ssh_port            = $centrify::params::ssh_port,
-  $ssh_x11             = $centrify::params::ssh_x11,
-  $ssh_banner          = $centrify::params::ssh_banner,
-  $groups_allow        = $centrify::params::groups_allow,
-  $users_allow         = $centrify::params::users_allow,
-  $adjoin_user         = $centrify::params::adjoin_user,
-  $adjoin_password     = $centrify::params::adjoin_password,
-  $adjoin_domain       = $centrify::params::adjoin_domain,
-  $adjoin_server       = $centrify::params::adjoin_server,
-  $private_group       = $centrify::params::private_group,
-  $primary_gid         = $centrify::params::primary_gid,
-  $auto_join           = $centrify::params::auto_join,
-  $maximumpasswordage  = $centrify::params::maximumpasswordage,
-  $minimumpasswordage  = $centrify::params::minimumpasswordage,
-  $lockoutduration     = $centrify::params::lockoutduration,
-  $lockoutbadcount     = $centrify::params::lockoutbadcount,
+  $dc_package_name        = $centrify::params::dc_package_name,
+  $dc_package_ensure      = $centrify::params::dc_package_ensure,
+  $dc_service_name        = $centrify::params::dc_service_name,
+  $dc_service_enable      = $centrify::params::dc_service_enable,
+  $dc_service_ensure      = $centrify::params::dc_service_ensure,
+  $ssh_package_name       = $centrify::params::ssh_package_name,
+  $ssh_package_ensure     = $centrify::params::ssh_package_ensure,
+  $ssh_service_name       = $centrify::params::ssh_service_name,
+  $ssh_service_enable     = $centrify::params::ssh_service_enable,
+  $ssh_service_ensure     = $centrify::params::ssh_service_ensure,
+  $auth_servers           = $centrify::params::auth_servers,
+  $ssh_permit_root        = $centrify::params::ssh_permit_root,
+  $ssh_port               = $centrify::params::ssh_port,
+  $ssh_x11                = $centrify::params::ssh_x11,
+  $ssh_banner             = $centrify::params::ssh_banner,
+  $groups_allow           = $centrify::params::groups_allow,
+  $users_allow            = $centrify::params::users_allow,
+  $adjoin_user            = $centrify::params::adjoin_user,
+  $adjoin_password        = $centrify::params::adjoin_password,
+  $adjoin_domain          = $centrify::params::adjoin_domain,
+  $adjoin_server          = $centrify::params::adjoin_server,
+  $private_group          = $centrify::params::private_group,
+  $primary_gid            = $centrify::params::primary_gid,
+  $auto_join              = $centrify::params::auto_join,
+  $maximum_password_age   = $centrify::params::maximum_password_age,
+  $minimum_password_age   = $centrify::params::minimum_password_age,
+  $lockout_duration       = $centrify::params::lockout_duration,
+  $lockout_bad_count      = $centrify::params::lockout_bad_count,
+  $merge_groups           = $centrify::params::merge_groups,
 ) inherits centrify::params {
 
   # validate parameters
@@ -88,6 +89,11 @@ class centrify (
   validate_bool($private_group)
   validate_string($primary_gid)
   validate_bool($auto_join)
+  validate_string($maximum_password_age)
+  validate_string($minimum_password_age)
+  validate_string($lockout_duration)
+  validate_string($lockout_bad_count)
+  validate_bool($merge_groups)
 
   # include classes for install, config and services
   include '::centrify::install'
